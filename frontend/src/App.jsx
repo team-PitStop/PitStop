f22381bdb3c30e64f622c00da0a29471c288ff9a
Angela Nano <angelanano@MacBookPro.lan>
2026-05-31 21:23:38 -0400

    Set up React Router and add Vite dependencies for US-5 routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Register from './Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Register from "./Register";
+import EditVehicleForm from "./EditVehicleForm";
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* Existing route - Miguel's Register page */}
+          <Route path="/" element={<Register />} />
+
+          {/* US-5: Edit Vehicle route */}
+          <Route path="/vehicles/:id/edit" element={<EditVehicleForm />} />
+
+          {/* Placeholder for Dylan's garage - he'll add this for US-4 */}
+          {/* <Route path="/garage" element={<Garage />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
